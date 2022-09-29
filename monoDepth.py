c7e0519d4e926866176008e979d602cc413ecb1d
GeniusAM <92140403+EmperoroftheMultiverse@users.noreply.github.com>
2022-09-29 09:37:34 +0530

    Update monoDepth.py

--- monoDepth.py
+++ monoDepth.py
@@ -1,3 +1,21 @@
+import cv2
+
+# distance from camera to object(face) measured
+# centimeter
+Known_distance = 76.2
+# width of face in the real world or Object Plane
+# centimeter
+Known_width = 14.3
+# Colors
+GREEN = (0, 255, 0)
+RED = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+# defining the fonts
+fonts = cv2.FONT_HERSHEY_COMPLEX
+# face detector object
+face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
 def face_data(image):
 
 	face_width = 0 # making face width to zero
@@ -21,32 +39,11 @@ def face_data(image):
 
 	# return the face width in pixel
 	return face_width
+
 # focal length finder function
 def FocalLength(measured_distance, real_width, width_in_rf_image):
     focal_length = (width_in_rf_image* measured_distance)/ real_width
     return focal_length
-# install opencv "pip install opencv-python"
-import cv2
-
-# distance from camera to object(face) measured
-# centimeter
-Known_distance = 76.2
-
-# width of face in the real world or Object Plane
-# centimeter
-Known_width = 14.3
-
-# Colors
-GREEN = (0, 255, 0)
-RED = (0, 0, 255)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-
-# defining the fonts
-fonts = cv2.FONT_HERSHEY_COMPLEX
-
-# face detector object
-face_detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 # focal length finder function
 def Focal_Length_Finder(measured_distance, real_width, width_in_rf_image):
@@ -62,30 +59,6 @@ def Distance_finder(Focal_Length, real_face_width, face_width_in_frame):
 
 	# return the distance
 	return distance
-
-
-def face_data(image):
-
-	face_width = 0 # making face width to zero
-
-	# converting color image to gray scale image
-	gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-	# detecting face in the image
-	faces = face_detector.detectMultiScale(gray_image, 1.3, 5)
-
-	# looping through the faces detect in the image
-	# getting coordinates x, y , width and height
-	for (x, y, h, w) in faces:
-
-		# draw the rectangle on the face
-		cv2.rectangle(image, (x, y), (x+w, y+h), GREEN, 2)
-
-		# getting face width in the pixels
-		face_width = w
-
-	# return the face width in pixel
-	return face_width
 
 
 # reading reference_image from directory
