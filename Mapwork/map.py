2cd647e60415e82d785fcb1df7357e88f1bb3b8f
Vinnie <114577963+Amvinnie@users.noreply.github.com>
2022-10-09 22:26:10 +0530

    Update map.py

--- Mapwork/map.py
+++ Mapwork/map.py
@@ -1,4 +1,5 @@
 import datetime
+from ssl import Options
 from tokenize import Double
 import pyttsx3
 import speech_recognition
@@ -6,34 +7,41 @@ import requests
 import bs4
 import selenium 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from time import sleep
-driver = webdriver.Chrome(executable_path="C:\chromedriver.exe")  # to open the chromebrowser
+driver = webdriver.Chrome("chromedriver")
 driver.get("https://www.google.com/maps/@23.232512,77.430784,12z")
-sleep(2)
 
 def searchplace():
-    place = driver.find_element_by_class_name("tactile-searchbox-input" )
-    place.send_keys("Funcity")
-    submit= driver.find_element_by_xpath("/html/body/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/button")
-    submit.click()
+ place = driver.find_element(by=By.CLASS_NAME, value=("tactile-searchbox-input"))
+ place.send_keys('Kanha Funcity') //destination
+ submit = driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/button"))
+ submit.click()
+ sleep(2)
 
-    searchplace()
-
-    def direction():
-        sleep(6)
-        direction= driver.find_element_by_xpath("/html/body/div[3]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[4]/div[1]/button")
-
+def direction():
+        sleep(2)
+        direction= driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[4]/div[1]/button"))
         direction.click()
+        sleep(2)
 
-        direction()
+        
 
-        def find():
-            sleep(5)
-            find= driver.find_element_by_xpath("/html/body/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/div[1]/div/input")
-            find.send_keys("D mart")
-            search_btn= driver.find_element_by_xpath("/html/body/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/button[1]")
+def find():
+            sleep(2)
+            find= driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/div[1]/div/input"))
+            find.send_keys("D mart") //currentlocation
+            search_btn= driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/button[1]"))
             search_btn.click()
+            walk = driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[2]/div/div/div/div[4]/button"))
+            walk.click()
+            
 
-            find()
+
+       
+searchplace()
+direction()
+find()
+
 
 
