d5fb1f037fe81891054fa71ec7d7d04e7134d1bd
GeniusAM <114599133+Alok-Munshi@users.noreply.github.com>
2022-10-15 23:42:38 +0530

    Update 1 to Mapwork
    Update 2 will be done as soon as a billing account is created

--- Mapwork/map.py
+++ Mapwork/map.py
@@ -1,38 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
-
-driver = webdriver.Chrome("chromedriver.exe")
+driver = webdriver.Chrome("chromedriver")
 driver.get("https://www.google.com/maps/@23.232512,77.430784,12z")
 
-<<<<<<< Updated upstream
-
-def searchplace():
-    place = driver.find_element(by=By.CLASS_NAME, value=("tactile-searchbox-input"))
-    place.send_keys("Mount carmel school baghmugalia") #destination
-    submit = driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/button"))
-    submit.click()
-    sleep(3)
-
-def direction():
-    sleep(3)
-    direction= driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[4]/div[1]/button"))
-    direction.click()
-    sleep(3)
-
-def find():
-    sleep(3)
-    find= driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/div[1]/div/input"))
-    find.send_keys("Kanha Funcity")#current location
-    search_btn= driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/button[1]"))
-    search_btn.click()
-    walk = driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[2]/div/div/div/div[4]/button"))
-    walk.click()
-
-searchplace()
-direction()
-find()
-=======
 def btnclick(xpath):
 	f = True
 	while f:
@@ -63,4 +34,3 @@ def find(place1):
     find.send_keys(place1)# current
     btnclick("/html/body/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/button[1]")
     btnclick("/html/body/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[2]/div/div/div/div[4]/button")
->>>>>>> Stashed changes
