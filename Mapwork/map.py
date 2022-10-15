6b6371d7adbc7dd2eb1d9b285febca76ed211e3a
Vinnie <114577963+Amvinnie@users.noreply.github.com>
2022-10-15 08:58:25 +0530

    Update map.py

--- Mapwork/map.py
+++ Mapwork/map.py
@@ -9,40 +9,33 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
-driver = webdriver.Chrome("chromedriver")
+
+driver = webdriver.Chrome("chromedriver.exe")
 driver.get("https://www.google.com/maps/@23.232512,77.430784,12z")
 
-def searchplace():
- place = driver.find_element(by=By.CLASS_NAME, value=("tactile-searchbox-input"))
- place.send_keys('Kanha Funcity')
- submit = driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/button"))
- submit.click()
- sleep(2)
+
+def searchplace(place2):
+    place = driver.find_element(by=By.CLASS_NAME, value=("tactile-searchbox-input"))
+    place.send_keys(place2) 
+    submit = driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/button"))
+    submit.click()
+    sleep(3)
 
 def direction():
-        sleep(2)
-        direction= driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[4]/div[1]/button"))
-        direction.click()
-        sleep(2)
+    sleep(3)
+    direction= driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[4]/div[1]/button"))
+    direction.click()
+    sleep(3)
 
-        
+def find(place1):
+    sleep(3)
+    find= driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/div[1]/div/input"))
+    find.send_keys(place1)
+    search_btn= driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/button[1]"))
+    search_btn.click()
+    walk = driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[2]/div/div/div/div[4]/button"))
+    walk.click()
 
-def find():
-            sleep(2)
-            find= driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/div[1]/div/input"))
-            find.send_keys("D mart")
-            search_btn= driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[3]/div[1]/div[1]/div[2]/button[1]"))
-            search_btn.click()
-            walk = driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[3]/div[1]/div[2]/div/div[2]/div/div/div/div[4]/button"))
-            walk.click()
-        
-def detailsread():
-            sleep(2)
-            details = driver.find_element(by=By.XPATH, value=("/html/body/div[3]/div[9]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[4]/div[1]/div[1]/div[3]/div[4]/button"))
-            details.click()
-
-       
-searchplace()
+searchplace(place2)#Destination
 direction()
-find()
-detailsread()
+find(place1)#current location
